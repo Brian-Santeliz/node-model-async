62d35c7b40bd422cd319a3ba6cb5638e8de7f273
Brian <blsvsanteliz@gmail.com>
2020-11-09 15:53:41 -0400

    Add promises

--- async.js
+++ async.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
 const util = require('util');
 
-const init = async()=>{
-try{
-	//soporta promesa al metodo de leer
-	const read = util.promisify(fs.readFile);
-	//le el archibo
-	console.log("Ejemplo")
-	const response = await read("Hola.txt", "utf-8");	
-	console.log(response)
+	const readPromise = util.promisify(fs.readFile)
+
+	console.log("Empezando a leert")
+	readPromise("Hola.txt", "utf-8")
+		.then(console.log)
+		.catch(console.log)
+
+	// CALLBACK
+	// fs.readFile("Hola.txt", "utf-8", (error, respuesta)=>{
+	//	console.log(respuesta)
+	//})
+		
+	console.log("Ya termino")
 	console.log(process.uptime());
 
-}catch(e){
-	console.log(e);
-}
-}
 
-init();
+
+
+// bloquea la ejecucion del codigo ASYNC-AWAIT
+// const response = await readPromise("Hola.txt", "utf-8")
+// console.log(response)
